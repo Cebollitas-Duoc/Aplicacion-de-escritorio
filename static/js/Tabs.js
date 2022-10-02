bcc030d8a969294c9f4fd51fc91301f0587cb3cc
AndresGroselj <61983866+AndresGroselj@users.noreply.github.com>
2022-10-02 20:35:30 -0300

    tabs

---
+++ static/js/Tabs.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', async () =>{
+    tabs = await document.querySelectorAll("#sideBar a.nav-link");
+    tabs.forEach(element => {
+        element.addEventListener("click", async () => {
+            //element.innerHTML = element.getAttribute("tab")
+            currentTab = await document.querySelector("#sideBar a.active");
+            currentTab.classList.remove("active");
+            element.classList.add("active");
+            showTab(element.getAttribute("tab"))
+        });
+    });
+})
+
+async function showTab(tabid){
+    currentTab = await document.querySelector(".tab:not(.d-none)");
+    currentTab.classList.add("d-none");
+    tab = await document.getElementById(tabid);
+    tab.classList.remove("d-none");
+    console.log(tabid)
+}
