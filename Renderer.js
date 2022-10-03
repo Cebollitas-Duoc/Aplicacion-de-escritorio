40d26513719d844fd490529cb099d271b6974040
AndresGroselj <61983866+AndresGroselj@users.noreply.github.com>
2022-10-02 22:40:20 -0300

    mini profile

--- Renderer.js
+++ Renderer.js
@@ -1,21 +1,48 @@
+const { app } = require('electron')
 const fs = require('fs-extra');
 const nunjucks = require('nunjucks')
+const API = require('./API');
+
+async function getMiniProfileData(){
+    if (app.isPackaged)
+        var webDomain = "http://www.mrmeme.cl";
+    var webDomain = "http://localhost:8080";
+    profileData = await API.getSessionProfile()
+
+    if (profileData["ValidSession"]){
+
+        username = profileData["Name"]
+        lastName = profileData["LastName"]
+        data = {
+            "isLogged": true,
+            "usrName": `${username} ${lastName}`,
+            "usrImg": `${webDomain}${profileData["Picture"]}`,
+        }
+    }
+    else{
+        data = {
+            "isLogged": false,
+            "usrName": "",
+            "usrImg": `${webDomain}/img/profiles/default.png}`,
+        }
+    }
+    return data
+}
 
 class Renderer {
     static win;
 
     static async render(template, context = {}){
-        console.log("rendering")
-        console.log(template)
         const cacheFile = "templates/Cache.html"
         function writeCache(s){
             fs.writeFileSync(cacheFile, s, 'utf8', function (err) {
                if (err) return console.log(err);
             });
         }
-    
-        const universalContext = {}
-        const html = nunjucks.render("templates/"+template, context + universalContext);
+        
+        const universalContext = await getMiniProfileData()
+        console.log(Object.assign({}, context, universalContext))
+        const html = nunjucks.render("templates/"+template, Object.assign({}, context, universalContext));
         
         writeCache(html)
         await this.win.loadFile(cacheFile);
