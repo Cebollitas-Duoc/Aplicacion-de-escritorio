6bb7c509dd7e238672de5f410c821c262661e6b4
AndresGroselj <61983866+AndresGroselj@users.noreply.github.com>
2022-10-25 22:49:32 -0300

    show & select services

--- static/js/tabs/dpto/DptoServiceManager.js
+++ static/js/tabs/dpto/DptoServiceManager.js
@@ -4,16 +4,41 @@ document.addEventListener('DOMContentLoaded', async () =>{
 
 class DptoServiceManager{
     static popup;
-    static dd_category;
+    static serviceContainer;
+    static dd_categoryAdd;
+    static dd_categoryEdit;
     static services = {}
-    
+    static selectedSrv;
+    static selectedSrvId;
     
     static initiate(){
         this.popup = document.querySelector("#tab-departmentManager .popup.service");
-        this.dd_category = document.querySelector("#tab-departmentManager .popup.service .serviceCategory");
+        this.serviceContainer = document.querySelector("#tab-departmentManager .popup.service .service-container");
+        this.dd_categoryAdd   = document.querySelector("#tab-departmentManager .popup.service .serviceCategory.addService");
+        this.dd_categoryEdit  = document.querySelector("#tab-departmentManager .popup.service .serviceCategory.editService");
 
         DptoCategoryServiceManager.setCategorys()
     }
+
+    static cardTemplate = `
+    <div id="dptosrv-<<id>>" class="card" onclick="DptoServiceManager.selectSrv(<<id>>)">
+        <div class="card-body">
+            <div class="container text-center align-middle">
+                <div class="row">
+                    <div class="col-4">
+                        <<category>>
+                    </div>
+                    <div class="col-4">
+                        Estado: <<state>>
+                    </div>
+                    <div class="col-4">
+                        Cantidad: <<cantidad>>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
 
     static async showPopup(){
         this.setServices();
@@ -22,7 +47,12 @@ class DptoServiceManager{
     }
 
     static async setServices(){
+        this.serviceContainer.innerHTML = ""
         this.services = await this.getDptoService(DepartmentManager.input_id.value)
+        this.services.forEach(async (srv) => {
+            var card = await this.createSrvCard(srv)
+            appendStringElement(this.serviceContainer, card)
+        });
     }
 
     static async getDptoService(Id_Dpto){
@@ -43,16 +73,67 @@ class DptoServiceManager{
 
         return JSON.parse(r);
     }
+
+    static async createSrvCard(srv){
+        srv = await this.getSrvFormattedData(srv);
+        var card = this.cardTemplate;
+        card = card.replaceAll("<<id>>", srv.Id_Service)
+        card = card.replace("<<category>>", srv.category)
+        card = card.replace("<<state>>",    srv.status)
+        card = card.replace("<<cantidad>>", srv.Cantidad)
+
+        return card.toString()
+    }
+
+    static async getSrvFormattedData(srv){
+        function getStatus(srv){
+            switch (srv.Id_Estado) {
+                case 0:
+                    return "Activo"
+                case 1:
+                    return "Inactivo"
+                default:
+                    return srv.Id_Estado
+            }
+        }
+        function getCategorys(srv){
+            const catid = srv.Id_ServiceCategory
+            const category = DptoCategoryServiceManager.categorys.find(
+                cat => cat.Id_Categoria == catid);
+            if (category == undefined) return undefined;
+            return category.Descripcion;
+        }
+
+        if (DptoCategoryServiceManager.categorys == {})
+            await DptoCategoryServiceManager.setCategorys();
+
+        srv.status   = getStatus(srv)
+        srv.category = getCategorys(srv)
+
+        return srv
+    }
+
+    static selectSrv(dptosrvid){
+        if (this.selectedSrv != undefined)
+        this.selectedSrv.classList.remove("selected");
+
+        this.selectedSrvId = dptosrvid
+        this.selectedSrv = document.querySelector(`#dptosrv-${dptosrvid}`);
+
+        this.selectedSrv.classList.add("selected");
+    }
 }
 
 class DptoCategoryServiceManager{
     static categorys = {}
     static async setCategorys(){
         this.categorys = await this.getCategorys()
-        DptoServiceManager.dd_category.innerHTML = ""
+        DptoServiceManager.dd_categoryAdd.innerHTML = ""
+        DptoServiceManager.dd_categoryEdit.innerHTML = ""
         this.categorys.forEach(async (category) => {
             var option = `<option value="${category.Id_Categoria}">${category.Descripcion}</option>`
-            appendStringElement(DptoServiceManager.dd_category, option)
+            appendStringElement(DptoServiceManager.dd_categoryAdd, option)
+            appendStringElement(DptoServiceManager.dd_categoryEdit, option)
         });
     }
 
